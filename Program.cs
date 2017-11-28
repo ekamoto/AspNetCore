685614fac2c5de813fbe2c7a0f466fd9cec4764a
ekamoto <ekamoto.leandro@gmail.com>
2017-11-27 23:16:59 -0300

    Start

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var host = new WebHostBuilder().
+                        UseKestrel().
+                        Configure(app => {
+                            app.Run(context => context.Response.WriteAsync("Criando um Host: Leandro Shindi Ekamoto"));
+                        }).Build();
+            host.Run();    
+        }
+    }
+}
